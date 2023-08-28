315471c87fcf2bb880dc6db57b5ec80749d7ba27
INYONG KIM <ohnyong87@gmail.com>
2023-08-28 18:45:50 +0900

    refactor : Move RequestController package

--- src/main/java/com/sparta/springmvc/request/RequestController.java
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -1,4 +1,4 @@
-package com.sparta.springmvc.controller;
+package com.sparta.springmvc.request;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
